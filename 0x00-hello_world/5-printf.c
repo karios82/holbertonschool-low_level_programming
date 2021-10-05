abeed4d429da27035eb0152e9593eba5ae12edbb
Wilson Nova <jwnovac@gmail.com>
2021-10-04 23:48:14 -0700

    Sixth Commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -13,11 +13,11 @@
  *     */
 
 int main(void)
-	
+
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return (0);
+	return (0);
 
-}}
+}
