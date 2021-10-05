1e4d4313607fd39a9d18bbc2509b2138fbee2e8b
Wilson Nova <jwnovac@gmail.com>
2021-10-04 23:47:06 -0700

    Fifth Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,11 +13,11 @@
  *     */
 
 int main(void)
-	
+
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
-}}
+}
